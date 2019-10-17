3fa03569699e172810e87d1d673845edb9058c4a
randyajackson <rajackson.dev@gmail.com>
2019-10-17 11:36:13 -0600

    10/17/19 11:30a

---
+++ Javascript/pure functions/code.js
@@ -0,0 +1,15 @@
+// Pure functions produce deterministic, determinable code.
+
+// Function is only pure if given the same input, it produces the same output and it produces no side effects.
+// A side effect is a change to the state of the application
+
+// If a function is pure, it operates outside of outside state
+
+// They are good for parallel Processing 
+
+// in math, given x = 1 + ( 2 * 4 )
+// 2 * 4 can be replaced with 8 and it has no effect on x or 1
+
+// A referentially transparent method operates like multiply(2,4), 2*4 is replaced with the return value
+
+//
