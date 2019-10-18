7fdbd1e7aac7343c44b6f16b69fd99b2ec53e514
randyajackson <rajackson.dev@gmail.com>
2019-10-18 13:30:43 -0600

    10/18/19 1:30p

--- Javascript/object oriented/code.js
+++ Javascript/object oriented/code.js
@@ -1,5 +1,5 @@
 //prototype delegation
-//object search its entir protype chain for a given property.
+//object search its entire protype chain for a given property.
 
 //factory objects
 
@@ -85,5 +85,56 @@ LiliConstructor.intro();
 
 console.log('LiliConstructor is an instanceOf dog? ');
 console.log(LiliConstructor instanceof Dog);
+
+//here we are not creating a Dog class
+//there is an exploitation of prototype delegation happening
+//intro is still being created on every new object
+//-----------------------------------------------------------
+header("Pseudo Classical Pattern");
+//prototypal delegation
+//LiliConstructor.intro()
+//-it first looks for the prototype on itself
+//-if that doesn't exist, it looks at available properties of its prototype to see if intro exitst
+//-it does this until top level Object.prototype
+//"Prototype chain"
+
+function Doggy(name, age, breed)
+{
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+    this.isCute = "Yes, cute by default.";
+}
+
+Doggy.prototype.intro = function() { console.log(this.name + " is a "+ this.breed + " and is " + this.age + " years old."); } ;
+
+let bb = new Doggy("Lili", 9, "Weenie");
+let qt = new Doggy("Bacon Boy", 9, "Labrodor");
+
+bb.intro();
+qt.intro();
+
+bb.isCute = "Yes, very.";
+//qt.isCute = "No, nuh-uh.";
+
+Doggy.prototype.intro = function() { console.log(this.name + " is a "+ this.breed + " and is " + this.age + " years old. Is the dog cute? " + this.isCute); } ;
+
+bb.intro();
+qt.intro();
+
+console.log( Object.getPrototypeOf(bb) );
+
+//new creates a new object, assigns some state, and returns the object
+//now we use intro as a shared prototype between the objects
+//intro becomes a prototype of all new objects made from Doggy
+//bb.intro() is called and sees that bb does not have this method
+//it then goes up the prototype chain which it finds intro() on Doggy.prototype
+//.prototype keyword is required
+
+header("Object Linked to Other Object Pattern");
+
+
+
+
 
 
