451fe19970e70e15348db8f1a42b15f5d29b84b4
randyajackson <rajackson.dev@gmail.com>
2019-10-18 11:04:41 -0600

    10/18/19 11:05a

---
+++ Javascript/object oriented/code.js
@@ -0,0 +1,38 @@
+//prototype delegation
+//object search its entir protype chain for a given property.
+
+//factory objects
+
+//use factory functions to create objects of similar type
+
+function makeDog(name, age, breed)
+{
+    return( 
+        {
+            name: name, 
+            age: age, 
+            breed: breed, 
+            isCute: "yes",
+            intro: function() { console.log(this.name + " is a "+ this.breed + " and is " + this.age + " years old."); }
+        }
+    );
+}
+
+let Lili = makeDog("Lili", 9, "Wiener");
+let Baby = makeDog("Baby", 3, "Labrador");
+
+Lili.intro();
+Baby.intro();
+
+Lili.breed = "Little baby";
+
+Lili.intro();
+
+Lili.name = "Liliana";
+
+Lili.intro();
+
+if(Lili.isCute == Baby.isCute)
+{
+    console.log("All the dogs are cute.");
+}
